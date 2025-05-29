9936585b5f401446da59290a190c18f95078ce93
sathish-kumar-repo <sathishkumar.mailhub@gmail.com>
2025-05-29 12:54:15 +0530

    dfd

--- 01 Basic Program/App.java
+++ 01 Basic Program/App.java
@@ -1,9 +1,6 @@
 class App{
     public static void main(String args[]) {
         System.out.println("Hello World");
-        for (int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
-        }
     }
 }
 
