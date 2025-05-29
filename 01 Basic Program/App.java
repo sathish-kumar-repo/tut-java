08fcbb437c8d8d7be9206d11499ce28db6e705f4
sathish-kumar-repo <sathishkumar.mailhub@gmail.com>
2025-05-29 12:53:48 +0530

    updated

--- 01 Basic Program/App.java
+++ 01 Basic Program/App.java
@@ -1,4 +1,4 @@
-class index{
+class App{
     public static void main(String args[]) {
         System.out.println("Hello World");
         for (int i = 0; i < args.length; i++) {
--- 02 Command lin arg/index.java
+++
@@ -1,8 +0,0 @@
-class index{
-    public static void main(String args[]){
-
-        for(int i=0;i<args.length;i++){
-             System.out.println(args[i]);
-        }
-    }
-}
