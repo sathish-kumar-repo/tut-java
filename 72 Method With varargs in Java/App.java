0011ab84395e9aa115fd73691c87c767604642b5
sathish-kumar-repo <sathishkumar.mailhub@gmail.com>
2025-06-05 18:49:05 +0530

    dfdf

--- 72 Method With varargs in Java/App.java
+++ 72 Method With varargs in Java/App.java
@@ -1,13 +1,18 @@
 //Method With varargs in Java
 
 public class App {
-    // Method With Varargs in Java
-    public static void getNames(String... names) {
-        for (String name : names)
-            System.out.println(name);
+    // Method with varargs (int...)
+    static void printNumbers(int... numbers) {
+        System.out.println("Number of arguments: " + numbers.length);
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 
     public static void main(String args[]) {
-        getNames("Ram", "Sam", "Ravi", "Kumar", "sara");
+        printNumbers(); // no arguments
+        printNumbers(10); // one argument
+        printNumbers(1, 2, 3, 4, 5); // multiple arguments }
     }
 }
